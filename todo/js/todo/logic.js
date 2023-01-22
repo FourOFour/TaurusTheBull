825b07c35e77dda39a1d55770fe828b3833b1ed0
TaurusTheBull <103037786+FourOFour@users.noreply.github.com>
2023-01-22 14:24:32 +0330

    Separating logic & render
    - Logic & Render are now separated
    - Logic handles , state array, event handlers
    - Render works base on state array change ( no watcher - it gets called on changed from logic )
    - Render only applies changes, instead of rendering everything
    - Render binds passed in event handlers

---
+++ todo/js/todo/logic.js
@@ -0,0 +1,355 @@
+function TodoLogic({id = 'app', shouldInit = true, presets = {}} = {}) {
+    // deafultPresets is rooted in this constructor that is why it comes very first thing 
+    var deafultPresets;
+    
+    // TODO:If i was using modular pattern using default as json or module would be much much cleaner
+    {
+        deafultPresets = {
+            appId: id,
+            appContainerClass: 'todo-container',
+            taskViewContainerClass: 'todo',
+            taskIdPrefix: 'todo-task-',
+            newTaskInputPlaceHolder: '+ Add task...',
+            newTaskClassPrefix: 'new-task-',
+            taskElementClassPrefix: 'task-element-',
+            taskElementClassEditPrefix: 'task-edit-element-',
+            taskOnEditClass: 'onedit',
+            taskDoneClass: 'done'
+        };
+
+        deafultPresets.newTaskClass = {
+                form: `${deafultPresets.newTaskClassPrefix}form`,
+                text: `${deafultPresets.newTaskClassPrefix}text`,
+                submit: `${deafultPresets.newTaskClassPrefix}submit`
+        };
+
+        deafultPresets.taskElementClass = {
+            text: `${deafultPresets.taskElementClassPrefix}text`,
+            toggleIsDone: `${deafultPresets.taskElementClassPrefix}toggleIsDone`,
+            edit: `${deafultPresets.taskElementClassPrefix}edit`,
+            remove: `${deafultPresets.taskElementClassPrefix}remove`
+        };
+        
+        deafultPresets.taskElementClassEdit = {
+            form : `${deafultPresets.taskElementClassEditPrefix}form`,
+            text : `${deafultPresets.taskElementClassEditPrefix}text`,
+            submit : `${deafultPresets.taskElementClassEditPrefix}submit`,
+            cancel : `${deafultPresets.taskElementClassEditPrefix}cancel`
+        };
+        
+        deafultPresets.taskElementClassEdit = {
+            form : `${deafultPresets.taskElementClassEditPrefix}form`,
+            text : `${deafultPresets.taskElementClassEditPrefix}text`,
+            submit : `${deafultPresets.taskElementClassEditPrefix}submit`,
+            cancel : `${deafultPresets.taskElementClassEditPrefix}cancel`
+        };
+    }
+
+    // example of default values
+    /*
+        defult: {
+            id: 'app',
+            shouldInit: true,
+            presets = {}
+        }
+    */
+
+    // Using var because they will be used through the enitre scope
+    // + Private variables (execpt app)
+    /*
+        In case someone wants to only make changes or edit
+        presets.newTaskClass.form ( for example )
+        they don't need to pass all properties
+        (if not it will be overwriten)
+        A bit seems too much code but gives us too much flexbility
+    */
+    presets.newTaskClass = Object.assign({}, deafultPresets.newTaskClass, presets.newTaskClass);
+    presets.taskElementClass = Object.assign({}, deafultPresets.taskElementClass, presets.taskElementClass);
+    presets.taskElementClassEdit = Object.assign({}, deafultPresets.taskElementClassEdit, presets.taskElementClassEdit);
+
+    // so we can over-write any preset we want
+    presets = Object.assign({}, deafultPresets, presets);
+
+    var state = {
+        newTaskContainer: null,
+        taskViewContainer: null,
+        tasksList: [],
+        counter: 0,
+        localStorage: window.localStorage,
+        render: null
+    };
+
+    /*
+        tasks[] example
+        [
+            {
+                id: Number,
+                value: String,
+                isDone: Boolean
+            }
+        ]
+    */
+
+    /*
+        {addTaskSubmit: function 'addTaskSubmit'}
+        to avoid any anonymous function error in future development
+    */
+    var eventsHandler = {
+        addTaskSubmit: function addTaskSubmit(event) {
+            var parent, input, value;
+    
+            {
+                parent = event.currentTarget.closest(`.${presets.newTaskClass.form}`);
+                input = parent.querySelector(`.${presets.newTaskClass.text}`);
+    
+                value = input.value;
+            }
+    
+            addTask({value});
+            input.value = '';
+    
+            event.preventDefault();
+        },
+        setTaskToEditBtnClick: function setTaskToEditBtnClick(event) {
+            var taskElement;
+
+            {
+                taskElement = event.currentTarget.closest('li');
+            }
+
+            state.render.setTaskToEdit({taskElement});
+            
+            event.preventDefault();
+        },
+        removeTaskBtnClick: function removeTaskBtnClick(event) {
+            var id;
+
+            {
+                let taskElement = event.currentTarget.closest('li');
+
+                id = clarifyElementId(taskElement.id);
+            }
+
+            removeTask(id);
+
+            event.preventDefault();
+        },
+        taskTextClick: function taskTextClick(event) {
+            var toggleIsDone;
+
+            {
+                let taskElement = event.currentTarget.closest('li');
+                toggleIsDone = taskElement.querySelector(`.${presets.taskElementClass.toggleIsDone}`);
+            }
+
+            toggleIsDone.click();
+
+            event.preventDefault();
+        },
+        taskEditSubmitBtnClick: function taskEditSubmitBtnClick(event) {
+            var taskElement, id, value, input;
+
+            {
+                taskElement = event.currentTarget.closest('li');
+
+                id = clarifyElementId(taskElement.id);
+
+                input = taskElement.querySelector(`.${presets.taskElementClassEdit.text}`);
+                value = input.value;
+            }
+
+            updateTask({id, value});
+            
+            state.render.resetTaskToEdit({taskElement});
+
+            event.preventDefault();
+        },
+        taskEditCancelBtnClick: function taskEditCancelBtnClick(event) {
+            var taskElement;
+
+            {
+                taskElement = event.currentTarget.closest('li');
+            }
+
+            state.render.resetTaskToEdit({taskElement});
+            
+            event.preventDefault();
+        },
+        toggleIsDoneClick: function toggleIsDoneClick(event) {
+            var taskElement, toggleIsDone, isDone, index, taskObject, id;
+
+            {
+                taskElement = event.currentTarget.closest('li');
+                toggleIsDone = taskElement.querySelector(`.${presets.taskElementClass.toggleIsDone}`);
+                
+                id = clarifyElementId(taskElement.id);
+
+                ({index, taskObject} = findTaskObjectWithId(id));
+
+                isDone = !taskObject.isDone;
+            }
+
+            updateTask({id, isDone});
+
+            // if the checkbox's state is changed, this content attribute does not reflect the change. 
+            // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/checkbox#attr-checked
+            // need the event default to change it's state
+            // event.preventDefault();
+        }
+    }
+    
+    var app = {};
+
+    function checkLocalStorage() {
+        var tasksList = JSON.parse(state.localStorage.getItem(`${presets.appId}TasksList`));
+
+        if (tasksList) {
+            tasksList.forEach(function({value, isDone}, i) {
+                addTask({value, isDone, storage: false});
+            });
+        }
+
+        updateView();
+    }
+
+    function updateLocalStorage() {
+        // Only keeping the 'value' and 'isDone'
+        var tasksList = [];
+
+        {
+            state.tasksList.forEach(function({value, isDone}, i) {
+                tasksList.push({
+                    value,
+                    isDone
+                })
+            });
+        }
+
+        state.localStorage.setItem(`${presets.appId}TasksList`, JSON.stringify(tasksList));
+    }
+
+    // Have a function to call on load for initialization or anywhere we want.
+    function init() {
+        state.render = new TodoRender({
+            eventsHandler,
+            presets
+        });
+        // addTask({value: 'salam1'});
+        // addTask({value: 'salam2'});
+        // addTask({value: 'salam3'});
+        // addTask({value: 'salam4'});
+        // addTask({value: 'salam5'});
+        // addTask({value: 'salam6'});
+        // addTask({value: 'salam7'});
+        // addTask({value: 'salam8'});
+        // addTask({value: 'salam9'});
+        // addTask({value: 'salam10'});
+        // addTask({value: 'salam11'});
+        // addTask({value: 'salam12'});
+        // addTask({value: 'salam13'});
+        // removeTask(4,5);
+        // updateTask({id:2, value:'bye'}, {id:3, isDone: true});
+        checkLocalStorage();
+    }
+
+    function createTaskObject({value, id, isDone}) {
+        var task = {
+            id,
+            value,
+            isDone
+        };
+
+        return task;
+    }
+
+    function generateNewId() {
+        return state.counter++;
+    }
+
+    function clarifyElementId(id) {
+        const reg = new RegExp(`${presets.taskIdPrefix}(\\d+)`);
+
+        // the id that comes here is string
+        return parseInt(id.match(reg)[1]);
+    }
+
+    // Incase we need to check
+    function isDuplicate(value) {
+        return state.tasksList.some(function(v) {
+            if (value == v.value) return true;
+        })
+    }
+
+    function addTask({value, isDone = false, storage = true}) {
+        var taskObject, id;
+    
+        {
+            id = generateNewId();
+            taskObject = createTaskObject({value, id, isDone});
+        }
+
+        state.tasksList.push(taskObject);
+
+        if (storage) {
+            updateLocalStorage();
+            updateView();
+        }
+    }
+
+    function findTaskObjectWithId(id) {
+        var index, taskObject;
+
+        {
+            taskObject = state.tasksList.find(function stateTasksListFind(item, i) {
+                if (item.id == id) {
+                    index = i;
+                    return true
+                };
+            });
+        }
+
+        return {index, taskObject};
+    }
+
+    function removeTask(...id) {
+        // Since its more favorite to use shallow copy :)
+        state.tasksList = state.tasksList.filter(function tasklistFilter(item) {
+            return !id.some(i => i == item.id);
+        });
+
+        updateLocalStorage();
+        updateView();
+    }
+
+    function updateTask(...updatedTasks) {
+        updatedTasks.forEach(function updatedTasksForEach(item) {
+            if (typeof item.id == 'string') item.id = parseInt(item.id);
+
+            var {index, taskObject} = findTaskObjectWithId(item.id);
+
+            state.tasksList.splice(index, 1, Object.assign({} , taskObject, item))
+        });
+
+        updateLocalStorage();
+        updateView();
+    }
+
+    // shouldInit will only be Boolean
+    if (shouldInit) init();
+
+    function updateView() {
+        state.render.updateView(state.tasksList);
+    }
+
+    Object.assign(app, {
+        init,
+        addTask,
+        removeTask,
+        updateTask,
+        updateView,
+        state
+    });
+   
+    // We chose what do we expose to app object instead of returning 'this'
+    return app;
+}
---
+++ todo/js/todo/render.js
@@ -0,0 +1,270 @@
+function TodoRender({
+        id = 'app',
+        shouldInit = true,
+        eventsHandler = {
+            // addTaskSubmit
+            // setTaskToEditBtnClick
+            // removeTaskBtnClick
+            // taskTextClick
+            // taskEditSubmitBtnClick
+            // taskEditCancelBtnClick
+            // toggleIsDoneClick
+        },
+        presets
+    } = {}) {
+
+    // Public API
+    var app = {}
+    
+    var state = {
+        newTaskContainer: null,
+        taskViewContainer: null,
+        liveTasksList: []
+    }
+
+    function init() {
+        createBasicStructure();
+    }
+
+    function createBasicStructure() {
+        var app = document.querySelector(`#${presets.appId}`),
+            ul, form;
+
+        {            
+            form = document.createElement('form');
+            state.newTaskContainer = form;
+            form.classList.add(`${presets.newTaskClass.form}`);
+
+            let input = document.createElement('input');
+            input.value = '';
+            input.setAttribute('type', 'text');
+            input.setAttribute('placeholder', `${presets.newTaskInputPlaceHolder}`);
+            input.classList.add(`${presets.newTaskClass.text}`);
+
+            let submit = document.createElement('input');
+            submit.value = '+';
+            submit.setAttribute('type', 'submit');
+            submit.classList.add(`${presets.newTaskClass.submit}`);
+            submit.addEventListener('click', eventsHandler.addTaskSubmit);
+
+            state.newTaskContainer.append(input, submit);
+        }
+        
+        {
+            ul = document.createElement('ul')
+            ul.classList.add(presets.taskViewContainerClass);
+            state.taskViewContainer = ul;
+        }
+
+        app.classList.add(`${presets.appContainerClass}`);
+        app.append(state.newTaskContainer, state.taskViewContainer);
+    }
+
+    function createTaskElement({value, id, isDone}) {
+        var li = document.createElement('li');
+
+        {
+            let span = document.createElement('span');
+            let txt = document.createTextNode(value);
+            let remove = document.createElement('input');
+            let edit = document.createElement('input');
+            let toggleIsDone = document.createElement('input');
+
+            edit.value = 'edit';
+            edit.classList.add(presets.taskElementClass.edit);
+            edit.setAttribute('type', 'button');
+            edit.addEventListener('click', eventsHandler.setTaskToEditBtnClick);
+
+            remove.value = 'x';
+            remove.setAttribute('type', 'button');
+            remove.addEventListener('click', eventsHandler.removeTaskBtnClick);
+            
+            remove.classList.add(presets.taskElementClass.remove);
+
+            span.classList.add(presets.taskElementClass.text);
+            span.append(txt);
+            span.addEventListener('click', eventsHandler.taskTextClick);
+
+            toggleIsDone.checked = isDone;
+            toggleIsDone.setAttribute('type', 'checkbox');
+            toggleIsDone.classList.add(presets.taskElementClass.toggleIsDone);
+            toggleIsDone.addEventListener('click', eventsHandler.toggleIsDoneClick);
+
+            if (isDone) li.classList.add(presets.taskDoneClass);
+
+            li.classList.add('animate-flipinx');
+            li.append(toggleIsDone, span, edit, remove);
+            li.id = `${presets.taskIdPrefix}${id}`;
+        }
+
+        return li;
+    }
+
+    function updateView(tasksList = []) {
+        state.liveTasksList.forEach(function stateLiveTasksListForEach(item, index) {
+            if (!tasksList.some(sItem => item.id == sItem.id)) {
+                removeTask(item.id);
+            }
+        });
+        
+        tasksList.forEach(function taskListForEach(item, index) {
+            let isLive = state.liveTasksList.some(function stateLiveTasksListSome(sItem) {
+                if (sItem.id == item.id) {
+                    // console.log({iNew: item, iOld: sItem});
+                    if (isChanged({iNew: item, iOld: sItem})) {
+                        updateTask(item);
+                    }
+
+                    return true;
+                }
+            });
+            if (!isLive) {
+                addTask(item);
+            }
+        });
+
+        /*
+            Since state.liveTasksList just acts as temp history so we can
+            compare new tasksList to live one with out any extra code
+            and have to use Array.prototype.concat() for example to create new array or return a
+            new shallowcopy of it so tasksList and state.liveTasksList both don't point toward one point
+            in memory (since we use method in logic to call the render to act and detect the changes)
+            we can't let prev or live state lose its active or live value by passing the value of tasksList
+            directly, might be too much to info but that's the reason we can't asign them both to same value.
+        */
+        state.liveTasksList = tasksList.concat([]);
+    }
+
+    function isChanged({iNew, iOld} = {}) {
+        // if the points amount is lower then max ( 2 atm ) means it has changed
+        // that is why no break at the switch
+        var point = 0, reg;
+
+        // reg = new RegExp(`^${iNew.value}$`);
+
+        if (Object.is(iNew.value, iOld.value)) point++;
+        if (Object.is(iNew.isDone, iOld.isDone)) point++;
+
+        return point < 2;
+    }
+    
+    function addTask({value, id, isDone}) {
+        var task = createTaskElement({value, id, isDone});
+
+        state.taskViewContainer.append(task);
+    }
+    
+    function removeTask(id) {
+        var task = state.taskViewContainer.querySelector(`#${presets.taskIdPrefix}${id}`);
+        
+        {
+            task.classList.add('animate-flipoux');
+
+            setTimeout(function IIFE() {
+                task.remove();
+            }, 1000);
+        }
+    }
+    
+    function updateTask({value, id, isDone} = {}) {
+        var taskElement, span, txt, toggleIsDone;
+
+        {
+            taskElement = state.taskViewContainer.querySelector(`#${presets.taskIdPrefix}${id}`);
+        }
+        
+        if (value) {
+            span = taskElement.querySelector(`.${presets.taskElementClass.text}`);
+            txt = span.firstChild;
+            txt.nodeValue = value;
+        }
+
+        // isDone can be falsy, since is update function,
+        // we don't wana run this section when only value is going to change
+        if (isDone != undefined) {
+            toggleIsDone = taskElement.querySelector(`.${presets.taskElementClass.toggleIsDone}`);
+            toggleIsDone.checked = isDone
+            
+            // now we check for boolean value
+            if (isDone) taskElement.classList.add(presets.taskDoneClass);
+            else taskElement.classList.remove(presets.taskDoneClass);
+        };
+    }
+    
+    function setTaskToEdit({taskElement}) {
+        var form, input, submit, cancel, remove, edit, span, toggleIsDone, txtNode;
+        
+        {
+            remove = taskElement.querySelector(`.${presets.taskElementClass.remove}`);
+            edit = taskElement.querySelector(`.${presets.taskElementClass.edit}`);
+            span = taskElement.querySelector(`.${presets.taskElementClass.text}`);
+            toggleIsDone = taskElement.querySelector(`.${presets.taskElementClass.toggleIsDone}`);
+            txtNode = span.firstChild;
+
+            form = document.createElement('form');
+            form.classList.add(presets.taskElementClassEdit.form);
+
+            input = document.createElement('input');
+            input.setAttribute('type', 'text');
+            input.value = txtNode.nodeValue;
+            input.classList.add(presets.taskElementClassEdit.text);
+
+            submit = document.createElement('input');
+            submit.setAttribute('type', 'submit');
+            submit.value = 'done';
+            submit.addEventListener('click', eventsHandler.taskEditSubmitBtnClick);
+            submit.classList.add(presets.taskElementClassEdit.submit);
+
+            cancel = document.createElement('input');
+            cancel.setAttribute('type', 'button');
+            cancel.value = 'cancel';
+            cancel.addEventListener('click', eventsHandler.taskEditCancelBtnClick);
+            cancel.classList.add(presets.taskElementClassEdit.cancel);
+
+            span.hidden = true;
+            remove.hidden = true;
+            edit.hidden = true;
+            toggleIsDone.hidden = true;
+
+            form.append(input, cancel, submit);
+            taskElement.append(form);
+            taskElement.classList.add(`${presets.taskOnEditClass}`);
+        }
+    }
+
+    function resetTaskToEdit({taskElement}) {
+        var remove, edit, span, toggleIsDone, form;
+        
+        {
+            remove = taskElement.querySelector(`.${presets.taskElementClass.remove}`);
+            edit = taskElement.querySelector(`.${presets.taskElementClass.edit}`);
+            span = taskElement.querySelector(`.${presets.taskElementClass.text}`);
+            toggleIsDone = taskElement.querySelector(`.${presets.taskElementClass.toggleIsDone}`);
+            form = taskElement.querySelector(`.${presets.taskElementClassEdit.form}`);
+        }
+
+        form.remove();
+
+        span.hidden = false;
+        remove.hidden = false;
+        edit.hidden = false;
+        toggleIsDone.hidden = false;
+
+        taskElement.classList.remove(`${presets.taskOnEditClass}`);
+    }
+
+    if (shouldInit) init();
+
+    Object.assign(app, {
+        init,
+        updateTask,
+        removeTask,
+        addTask,
+        setTaskToEdit,
+        resetTaskToEdit,
+        updateView,
+        state
+    });
+
+    return app;
+}
